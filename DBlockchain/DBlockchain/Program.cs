26a36e7afdee60b05f12acf53071a197025b22f8
David-Mavrodiev <david_mavrodiev@abv.bg>
2018-02-26 17:43:13 +0200

    Add logo

--- DBlockchain/DBlockchain/Program.cs
+++ DBlockchain/DBlockchain/Program.cs
@@ -10,6 +10,8 @@ namespace DBlockchain
         {
             Console.Title = "DBlockchain";
 
+            DrawTitle();
+
             IKernel kernel = new StandardKernel(new NinjectBindings());
 
             var commandFabric = kernel.Get<ICommandFabric>();
@@ -20,6 +22,19 @@ namespace DBlockchain
 
                 commandFabric.RunCommand(input);
             }
+        }
+
+        public static void DrawTitle()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"____________ _            _        _           _       ");
+            Console.WriteLine(@"|  _  \ ___ \ |          | |      | |         (_)      ");
+            Console.WriteLine(@"| | | | |_/ / | ___   ___| | _____| |__   __ _ _ _ __  ");
+            Console.WriteLine(@"| | | | ___ \ |/ _ \ / __| |/ / __| '_ \ / _` | | '_ \ ");
+            Console.WriteLine(@"| |/ /| |_/ / | (_) | (__|   < (__| | | | (_| | | | | |");
+            Console.WriteLine(@"|___/ \____/|_|\___/ \___|_|\_\___|_| |_|\__,_|_|_| |_|");
+            Console.ResetColor();
+            Console.WriteLine(@"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
         }
     }
 }
